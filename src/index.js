bca129b1e638642448a76b0c62b7f9ba1637239a
Yauheni Ramankou <iromankov2010@gmail.com>
2020-02-25 18:52:45 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0){
+      return 0;
+    }
+    else{
+      return array.reduce((acc, currentItem) => {
+        return acc < currentItem ? acc : currentItem;
+    });
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array === undefined || array.length === 0){
+    return 0;
+  }
+  else{
+    return array.reduce((acc, currentItem) => {
+      return acc > currentItem ? acc : currentItem;
+  });
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let result;
+
+  if (array === undefined || array.length === 0){
+    return 0;
+  }
+  else{
+    let arrLen = array.length;
+    result = array.reduce((acc, currentItem) => {
+      return acc + currentItem;
+    });
+
+    return result / arrLen;
+  }
 }
